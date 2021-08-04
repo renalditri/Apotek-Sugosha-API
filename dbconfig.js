a80b535c8060172eb0f639b247105035497339de
Minka <renalditriananda@gmail.com>
2021-08-04 21:13:01 +0700

    config

--- dbconfig.js
+++ dbconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST:   "us-cdbr-east-04.cleardb.com",
-  USER: "b7ed1a40fd6519",
-  PASSWORD: "12ace2b6",
-  DB: "heroku_646285b168c51f9"
+  HOST:   "sql6.freemysqlhosting.net",
+  USER: "sql6429201",
+  PASSWORD: "YMTDxsGZns",
+  DB: "sql6429201"
 };
